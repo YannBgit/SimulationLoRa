084da916aee5d07d694dab2fd91f8e936889ae20
Adrien Soursou <asoursou@student.42.fr>
2022-05-17 19:25:34 +0200

    Ajout doc

--- src/lora.h
+++ src/lora.h
@@ -22,9 +22,37 @@ typedef struct s_simulation
 	double tempsEmission[MAX_ESSAIS];
 } Simulation;
 
+/**
+ * Traite un événement.
+ * @param sim La simulation.
+ * @param e L'événement à traiter.
+ */
 void Traitement_Event(Simulation *sim, const Evenement *e);
+
+/**
+ * Traite une collision.
+ * @param sim La simulation.
+ * @param e1 L'événement à traiter.
+ * @param e2 L'évènement à venir en collision.
+ */
 void Traitement_Collision(Simulation *sim, const Evenement *e1, Evenement *e2);
+
+/**
+ * Exécute une simulation.
+ * @param sim La simulation.
+ * @param showAll 0 pour n'afficher que la fin de la simulation, sinon affiche toutes les étapes.
+ */
 void Simulateur(Simulation *sim, int showAll);
 
+/**
+ * Initilise la simulation.
+ * @param sim La structure de la simulation à initialiser.
+ * @param K Le nombre de capteurs dans le réseau.
+ */
 void simulation_init(Simulation *sim, int K);
+
+/**
+ * Affiche l'état de la simulation.
+ * @param sim La simulation à afficher.
+*/
 void simulation_print(const Simulation *sim);
