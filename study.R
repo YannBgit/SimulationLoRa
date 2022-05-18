49d2ffcfd90156bfb9abd5b70b69d9100c4a426c
Adrien Soursou <asoursou@student.42.fr>
2022-05-18 17:50:32 +0200

    Ajout data

--- study.R
+++ study.R
@@ -12,7 +12,7 @@ plot_7_courbes_inter <- function(name1, name2)
 
     plot(lowess(data$T, data$P_col1, f = f_size),
          type = "l",
-         ylim = c(0, 0.5),
+         ylim = c(0, 0.2),
          pch = pch_id,
          cex = cex_size,
          xlab = "Temps de la simulation T",
@@ -31,11 +31,11 @@ plot_7_courbes_inter <- function(name1, name2)
 
     IC = t.test(data$P_col2, conf.level = 0.9)
 
-    abline(h = IC$conf.int[1], f = f_size, col = "black")
-    abline(h = IC$conf.int[2] - 0.01, f = f_size, col = "black")
+    abline(h = IC$conf.int[1] - 0.001, f = f_size, col = "black")
+    abline(h = IC$conf.int[2] + 0.001, f = f_size, col = "black")
 
-    legend("topright", legend = paste("Etat", c("e1", "e2", "e3", "e4", "e5", "e6", "e7"),
-    lty = "solid", col = c("blue", "green", "red", "coral3", "cyan4", "darkolivegreen4", "goldenrod")))
+    legend("topright", legend = paste("Etat", c("e1", "e2", "e3", "e4", "e5", "e6", "e7")),
+    lty = "solid", col = c("blue", "green", "red", "coral3", "cyan4", "darkolivegreen4", "goldenrod"))
 }
 
 # Tracer deux histogrammes du temps d'émission observé. Le premier concerne l'état e1, le deuxième
